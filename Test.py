f5165d8bcab3bd63d663438a742c705a4d159e14
peshal2404 <peshal@peshal.com.np>
2020-04-10 17:26:19 -0500

    save

--- Test.py
+++ Test.py
@@ -9,14 +9,5 @@ import sys
 f = os.popen('date')
 now = f.read()
 print ("Today is ", now)
-##subprocess.call(["cat", "/etc/resolv.conf"])
+subprocess.call(["cat", "/etc/resolv.conf"])
 
-cmdping = "ping -c2 peshal.com.np"
-p = subprocess.Popen(cmdping, shell=True, stderr=subprocess.PIPE)
-while True:
-    out = p.stderr.read(1)
-    if out == '' and p.poll() != None:
-        break
-    if out != '':
-         sys.stdout.write(out)
-         sys.stdout.flush()
