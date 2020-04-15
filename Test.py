a99145aba68b7693fd25fb49061c90b50f1f4ea1
peshal2404 <peshal@peshal.com.np>
2020-04-14 20:45:13 -0500

    save1

--- Test.py
+++ Test.py
@@ -2,30 +2,32 @@
 import mysql.connector
 import os
 import subprocess
+import requests 
 import sys
-
-#subprocess.call("date")
-
 f = os.popen('date')
 now = f.read()
 print ("Today is ", now)
 subprocess.call(["cat", "/etc/resolv.conf"])
-
 ###MySQL###
 
-import mysql.connector
+# import mysql.connector
+#
+# from mysql.connector import errorcode
+#
+# try:
+#   cnx = mysql.connector.connect(user='scott',
+#                                 database='employ')
+# except mysql.connector.Error as err:
+#   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+#     print("Something is wrong with your user name or password")
+#   elif err.errno == errorcode.ER_BAD_DB_ERROR:
+#     print("Database does not exist")
+#   else:
+#     print(err)
+# else:
+#   cnx.close()
 
-from mysql.connector import errorcode
+Home_Dir = /Users/peshal/Downloads
 
-try:
-  cnx = mysql.connector.connect(user='scott',
-                                database='employ')
-except mysql.connector.Error as err:
-  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-    print("Something is wrong with your user name or password")
-  elif err.errno == errorcode.ER_BAD_DB_ERROR:
-    print("Database does not exist")
-  else:
-    print(err)
-else:
-  cnx.close()
+
+
